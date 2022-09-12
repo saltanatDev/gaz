342294b52791c70b240dc8fb8c5cde03da12c54c
saltanatDev <saltanatkamalova558@gmail.com>
2022-09-12 17:07:16 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 100,
+    slideShadows: true,
+},
+    pagination: {
+    el: ".swiper-pagination",
+},
+});
+
+
+
+//       #2 Burger Menu...
+// codepen.io/victorfreire/pen/BjNveN
+//
+//
+// https://github.com/VictorFreire/Material-Design-Burger-Menu
